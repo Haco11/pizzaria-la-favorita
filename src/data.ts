dff3d3e45ec134b38ab5c603ecf80f91a6852a39
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-12-25 02:41:46 +0100

    adding data of pizzas

---
+++ src/data.ts
@@ -0,0 +1,125 @@
+const data = {
+  pizzaclass1: {
+    pizzas: [
+      {
+        title: "Vesuvio",
+        ingredients: ["Skinka"],
+      },
+      {
+        title: "Margherita",
+        ingredients: [""],
+      },
+      {
+        title: "Al Fungi",
+        ingredients: ["Champinjoner"],
+      },
+    ],
+  },
+  pizzaclass2: {
+    pizzas: [
+      {
+        title: "Räkpizza",
+        ingredients: ["Räkor"],
+      },
+      {
+        title: "Marinara",
+        ingredients: ["Musslor", "Räkor"],
+      },
+      {
+        title: "Opera",
+        ingredients: ["Skinka", "Tonfisk"],
+      },
+      {
+        title: "Capricciosa",
+        ingredients: ["Skinka", "Champinjoner"],
+      },
+      {
+        title: "La Bussola",
+        ingredients: ["Skinka", "Räkor"],
+      },
+      {
+        title: "Calzone",
+        ingredients: ["Skinka(inbakad)"],
+      },
+      {
+        title: "Toscana",
+        ingredients: ["Räkor", "Kronärtskocka"],
+      },
+      {
+        title: "Napolitano",
+        ingredients: ["Sardeller", "Vitlök", "Oliver"],
+      },
+      {
+        title: "Hawaii",
+        ingredients: ["Skinka", "Ananas"],
+      },
+      {
+        title: "Bolognese",
+        ingredients: ["Köttfärs", "Lök"],
+      },
+      {
+        title: "Al Tono",
+        ingredients: ["Tonfisk", "Lök"],
+      },
+      {
+        title: "Banana",
+        ingredients: ["Skinka", "Banan", "Curry"],
+      },
+      {
+        title: "Caciatore",
+        ingredients: ["Salami", "Lök"],
+      },
+      {
+        title: "Tropicana",
+        ingredients: ["Banan", "Curry", "Ananas"],
+      },
+      {
+        title: "Pepperoni pizza",
+        ingredients: ["Pepperonikorv"],
+      },
+    ],
+  },
+  pizzaamerika: {
+    pizzas: [
+      {
+        title: "Honululu",
+        ingredients: ["Oxfile", "Champinjoner", "Lök", "Tomater, Bearnaisesås"],
+      },
+      {
+        title: "Miami",
+        ingredients: ["Kyckling", "Banan", "Curry", "Ananas"],
+      },
+      {
+        title: "Kansas",
+        ingredients: ["Skinka", "Bacon", "Salami", "Tomater", "Lök", "Paprika"],
+      },
+      {
+        title: "Los Angeles",
+        ingredients: ["Pepperonikorv", "Lök", "Paprika", "Oliver", "Tomat"],
+      },
+    ],
+  },
+  pizzaclass3: {
+    pizzas: [
+      {
+        title: "Primavera",
+        ingredients: ["Bacon", "Salami", "Ägg", "Lök"],
+      },
+      {
+        title: "La Mira",
+        ingredients: ["Skinka", "Räkor", "Banan", "Lök"],
+      },
+      {
+        title: "Vegetariana",
+        ingredients: [
+          "Champinjoner",
+          "Lök",
+          "Paprika",
+          "Oliver",
+          "kronärtskocka",
+          "Ananas",
+        ],
+      },
+    ],
+  },
+};
