b8580fe6c93c7d6251af2d8399be11499e349151
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-12-26 17:07:57 +0100

    Adding arrowup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.scss";
+import ArrowUp from "./components/ArrowUp/ArrowUp";
 import Menu from "./components/Menu/Menu";
 
 function App() {
   return (
     <main>
+      <ArrowUp />
       <Menu />
     </main>
   );
---
+++ src/components/ArrowUp/ArrowUp.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import arrowUp from "../../assets/up-arrow.svg";
+import "./ArrowUp.scss";
+
+const ArrowUp = () => {
+  const [showArrow, setShowArrow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      // Adjust the threshold as needed
+      const scrollThreshold = 200;
+
+      if (scrollY > scrollThreshold) {
+        setShowArrow(true);
+      } else {
+        setShowArrow(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className={`sticky-arrow ${showArrow ? "show" : ""}`}>
+      <a href="#top" className="sticky-arrow--link">
+        <img src={arrowUp} alt="up arrow" />
+      </a>
+    </div>
+  );
+};
+
+export default ArrowUp;
