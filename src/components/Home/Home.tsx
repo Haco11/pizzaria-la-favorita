b527fa12edb0bf359690fa99dac2b5f8da14823b
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-12-27 23:52:34 +0100

    more content to home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,7 +5,12 @@ const Home = () => {
     <section className="home">
       <div className="container">
         <img src={Pizza} alt="pizza" />
-        <h2>Pizzaria La Favorita</h2>
+        <h2>
+          Pizzaria <span>La</span> Favorita
+        </h2>
+        <span className="additional-text">
+          Kvalitetsmat och fantastiska smaker
+        </span>
       </div>
     </section>
   );
