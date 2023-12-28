f23849a7f14cf2af7424135690ff7a8aba4761ff
Haci11 <46707887+Haci11@users.noreply.github.com>
2023-12-28 02:57:38 +0100

    changing width

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.scss";
+
 const Nav = () => {
-  return <div>Nav</div>;
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
+      <div className="container">
+        <div className="logo">
+          <span>Company Name</span>
+        </div>
+        <div className="contact-info">
+          <span>Telephone Nr</span>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
